5e82fa7b0517606ccc577e2d9d03fec2299f665e
David Bernheisel <david@bernheisel.com>
2019-08-21 17:47:14 -0400

    Refactor

--- lib/date_time.ex
+++ lib/date_time.ex
@@ -4,6 +4,7 @@ defmodule DateTimeParser.DateTime do
   import NimbleParsec
   import DateTimeParser.Combinators.Date
   import DateTimeParser.Combinators.DateTime
+  import DateTimeParser.Formatters, only: [format_token: 2, clean: 1]
 
   defparsec(
     :parse,
@@ -26,4 +27,40 @@ defmodule DateTimeParser.DateTime do
       us_date()
     ])
   )
+
+  def from_tokens(tokens) do
+    Map.merge(
+      NaiveDateTime.utc_now(),
+      clean(%{
+        year: format_token(tokens, :year),
+        month: format_token(tokens, :month),
+        day: format_token(tokens, :day),
+        hour: format_token(tokens, :hour) || 0,
+        minute: format_token(tokens, :minute) || 0,
+        second: format_token(tokens, :second) || 0,
+        microsecond: format_token(tokens, :microsecond) || {0, 0}
+      })
+    )
+  end
+
+  def timezone_from_tokens(tokens) do
+    with zone <- format_token(tokens, :zone_abbr),
+         offset <- format_token(tokens, :utc_offset),
+         true <- Enum.any?([zone, offset]) do
+      Timex.Timezone.get(offset || zone)
+    end
+  end
+
+  def from_naive_datetime_and_timezone(naive_datetime, nil), do: naive_datetime
+
+  def from_naive_datetime_and_timezone(naive_datetime, timezone_info) do
+    naive_datetime
+    |> DateTime.from_naive!("Etc/UTC")
+    |> Map.merge(%{
+      std_offset: timezone_info.offset_std,
+      utc_offset: timezone_info.offset_utc,
+      zone_abbr: timezone_info.abbreviation,
+      time_zone: timezone_info.full_name
+    })
+  end
 end
---
+++ lib/formatters.ex
@@ -0,0 +1,128 @@
+defmodule DateTimeParser.Formatters do
+  @moduledoc false
+
+  def format_token(tokens, :hour) do
+    case tokens |> find_token(:hour) do
+      {:hour, hour} ->
+        if tokens |> find_token(:am_pm) |> format == "PM" && hour < 12 do
+          hour + 12
+        else
+          hour
+        end
+
+      _ ->
+        nil
+    end
+  end
+
+  def format_token(tokens, :year) do
+    case tokens |> find_token(:year) |> format() do
+      nil ->
+        nil
+
+      year ->
+        year |> to_4_year() |> String.to_integer()
+    end
+  end
+
+  def format_token(tokens, token) do
+    tokens |> find_token(token) |> format()
+  end
+
+  defp find_token(tokens, find_me) do
+    Enum.find(tokens, fn
+      {token, _} -> token == find_me
+      _ -> false
+    end)
+  end
+
+  # If the parsed two-digit year is 00 to 49, then
+  #  - If the last two digits of the current year are 00 to 49, then the returned year has the same
+  #    first two digits as the current year.
+  #  - If the last two digits of the current year are 50 to 99, then the first 2 digits of the
+  #    returned year are 1 greater than the first 2 digits of the current year.
+  # If the parsed two-digit year is 50 to 99, then
+  #  - If the last two digits of the current year are 00 to 49, then the first 2 digits of the
+  #    returned year are 1 less than the first 2 digits of the current year.
+  #  - If the last two digits of the current year are 50 to 99, then the returned year has the same
+  #    first two digits as the current year.
+  defp to_4_year(parsed_3yr) when byte_size(parsed_3yr) == 3 do
+    [current_millenia | _rest] =
+      DateTime.utc_now()
+      |> Map.get(:year)
+      |> Integer.digits()
+
+    "#{current_millenia}#{parsed_3yr}"
+  end
+
+  defp to_4_year(parsed_2yr) when byte_size(parsed_2yr) == 2 do
+    [current_millenia, current_century, current_decade, current_year] =
+      DateTime.utc_now()
+      |> Map.get(:year)
+      |> Integer.digits()
+
+    parsed_2yr = String.to_integer(parsed_2yr)
+    current_2yr = String.to_integer("#{current_decade}#{current_year}")
+
+    cond do
+      parsed_2yr < 50 && current_2yr < 50 ->
+        "#{current_millenia}#{current_century}#{parsed_2yr}"
+
+      parsed_2yr < 50 && current_2yr >= 50 ->
+        [_parsed_millenia, parsed_century] =
+          [current_millenia, current_century]
+          |> Integer.undigits()
+          |> Kernel.+(1)
+          |> Integer.digits()
+
+        "#{current_millenia}#{parsed_century}#{parsed_2yr}"
+
+      parsed_2yr >= 50 && current_2yr < 50 ->
+        [parsed_millenia, parsed_century] =
+          [current_millenia, current_century]
+          |> Integer.undigits()
+          |> Kernel.-(1)
+          |> Integer.digits()
+
+        "#{parsed_millenia}#{parsed_century}#{parsed_2yr}"
+
+      parsed_2yr >= 50 && current_2yr >= 50 ->
+        "#{current_millenia}#{current_century}#{parsed_2yr}"
+    end
+  end
+
+  defp to_4_year(parsed_year), do: parsed_year
+
+  def format({:microsecond, value}) do
+    val = value |> to_string |> String.slice(0, 6)
+    {
+      val |> String.pad_trailing(6, "0") |> String.to_integer(),
+      val |> byte_size()
+    }
+  end
+
+  def format({:zone_abbr, value}), do: String.upcase(value)
+  def format({:utc_offset, offset}), do: to_string(offset)
+  def format({:year, value}), do: to_string(value)
+  def format({:am_pm, value}), do: String.upcase(value)
+  def format({_, value}) when is_integer(value), do: value
+  def format({_, value}), do: String.to_integer(value)
+  def format(_), do: nil
+
+  def clean(string) when is_binary(string) do
+    string
+    |> String.trim()
+    |> String.replace(" @ ", "T")
+    |> String.replace(~r{[[:space:]]+}, " ")
+    |> String.replace(" - ", "-")
+    |> String.replace("//", "/")
+    |> String.replace(~r{=|"|'|,|\\}, "")
+    |> String.downcase()
+  end
+
+  def clean(%{} = map) do
+    map
+    |> Enum.reject(fn {_k, v} -> is_nil(v) end)
+    |> Enum.into(%{})
+  end
+end
