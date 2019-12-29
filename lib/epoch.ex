10b62c70200a20581736f0b449b5913e6f10ec21
tmr08c <tmr08c@gmail.com>
2019-12-29 14:30:13 -0500

    Refactors `Epoch.parse`.
    Extracts the functionality for parsing seconds and subseconds into their
    own functions.

--- lib/epoch.ex
+++ lib/epoch.ex
@@ -6,37 +6,60 @@ defmodule DateTimeParser.Epoch do
 
   def parse(%{"sign" => sign, "seconds" => raw_seconds, "subseconds" => raw_subseconds}) do
     is_negative = if sign == "-", do: true, else: false
+    has_subseconds = if raw_subseconds == "", do: false, else: true
 
-    with {seconds, ""} <- Integer.parse(raw_seconds) do
-      seconds = if is_negative, do: seconds * -1, else: seconds
-
-      parsed_epoch =
-        case(raw_subseconds) do
-          "" ->
-            {seconds, {0, 0}}
-
-          raw_subseconds ->
-            {subseconds, ""} = Float.parse("0.#{raw_subseconds}")
-            microseconds = (subseconds * :math.pow(10, 6)) |> trunc()
-
-            truncated_microseconds =
-              microseconds
-              |> Integer.digits()
-              |> Enum.take(@max_subsecond_digits)
-              |> Integer.undigits()
-
-            number_of_subsecond_digits = min(String.length(raw_subseconds), @max_subsecond_digits)
-
-            if is_negative do
-              truncated_microseconds = 1_000_000 - truncated_microseconds
-              {seconds - 1, {truncated_microseconds, number_of_subsecond_digits}}
-            else
-              {seconds, {truncated_microseconds, number_of_subsecond_digits}}
-            end
-        end
-
-      {:ok, [{@token_key, parsed_epoch}], nil, nil, nil, nil}
+    with {:ok, seconds} <- parse_seconds(raw_seconds, is_negative, has_subseconds),
+         {:ok, subseconds} <- parse_subseconds(raw_subseconds, is_negative) do
+      {:ok, [{@token_key, {seconds, subseconds}}], nil, nil, nil, nil}
     end
+  end
+
+  @spec parse_seconds(String.t(), boolean(), boolean()) :: {:ok, integer()}
+  defp parse_seconds(raw_seconds, is_negative, has_subseconds)
+
+  defp parse_seconds(raw_seconds, true, true) do
+    with {:ok, seconds} <- parse_seconds(raw_seconds, true, false) do
+      {:ok, seconds - 1}
+    end
+  end
+
+  defp parse_seconds(raw_seconds, true, false) do
+    with {:ok, seconds} <- parse_seconds(raw_seconds, false, false) do
+      {:ok, seconds * -1}
+    end
+  end
+
+  defp parse_seconds(raw_seconds, false, _) do
+    with {seconds, ""} <- Integer.parse(raw_seconds) do
+      {:ok, seconds}
+    end
+  end
+
+  @spec parse_subseconds(String.t(), boolean()) :: {:ok, {integer(), integer()}}
+  defp parse_subseconds(raw_subseconds, is_negative)
+
+  defp parse_subseconds("", _), do: {:ok, {0, 0}}
+
+  defp parse_subseconds(raw_subseconds, true) do
+    with {:ok, {truncated_microseconds, number_of_subsecond_digits}} <-
+           parse_subseconds(raw_subseconds, false) do
+      {:ok, {1 * :math.pow(10, 6) - truncated_microseconds, number_of_subsecond_digits}}
+    end
+  end
+
+  defp parse_subseconds(raw_subseconds, false) do
+    {subseconds, ""} = Float.parse("0.#{raw_subseconds}")
+    microseconds = (subseconds * :math.pow(10, 6)) |> trunc()
+
+    truncated_microseconds =
+      microseconds
+      |> Integer.digits()
+      |> Enum.take(@max_subsecond_digits)
+      |> Integer.undigits()
+
+    number_of_subsecond_digits = min(String.length(raw_subseconds), @max_subsecond_digits)
+
+    {:ok, {truncated_microseconds, number_of_subsecond_digits}}
   end
 
   def from_tokens(tokens) do
