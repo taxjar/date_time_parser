72a0b0a1091c99c66d41c712071dba0c46bf90c6
David Bernheisel <david@bernheisel.com>
2020-10-17 20:39:47 -0400

    Update deps

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule DateTimeParser.MixProject do
   use Mix.Project
-  @version "1.0.0"
+  @version "1.1.0"
 
   def project do
     [
@@ -60,16 +60,15 @@ defmodule DateTimeParser.MixProject do
   end
 
   defp deps() do
-    [
-      {:nimble_parsec, "~> 0.5.0", runtime: false},
-      {:exprof, "~> 0.2.0", only: :bench}
-    ]
+    [{:exprof, "~> 0.2.0", only: :bench}]
+    |> add_if({:nimble_parsec, "~> 0.5.0", runtime: false}, "< 1.6.0")
+    |> add_if({:nimble_parsec, "~> 1.0", runtime: false}, ">= 1.6.0")
     |> add_if({:timex, ">= 3.2.1"}, ">= 1.6.0")
     |> add_if({:timex, "< 3.2.1"}, "< 1.6.0")
     |> add_if({:gettext, "<= 0.16.1"}, "< 1.6.0")
     |> add_if({:benchee, "~> 1.0", only: [:bench], runtime: false}, ">= 1.6.0")
     |> add_if({:credo, "~> 1.1", only: [:dev, :test], runtime: false}, ">= 1.5.0")
-    |> add_if({:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false}, ">= 1.6.0")
+    |> add_if({:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false}, ">= 1.6.0")
     |> add_if({:ex_doc, "~> 0.20", only: :dev, runtime: false}, ">= 1.7.0")
   end
 
